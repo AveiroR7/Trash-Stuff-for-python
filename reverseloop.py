4a2e5e0f13553dc918700081b4dab3a7c68f6f1d
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2020-11-27 15:06:11 +0000

    Initial Commit

--- reverseloop.py
+++ reverseloop.py
@@ -1,7 +1,9 @@
 #WAP to reverse content of the given string by using while loop.
 
 s=input("Enter the String: ")
-while True:
-    print(s)
-    break
-
+output=''
+i=len(s)-1
+while i>=0:
+    output=output+s[i]
+    i=i-1
+print(output)
